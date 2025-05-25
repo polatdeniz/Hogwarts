45f7de43ae79177141e54c2f6f93e48e9fb486fa
polatdeniz <polat.denizz@hotmail.com>
2025-05-25 20:55:35 +0300

    Add project files.

---
+++ HogwartsWeb/Data/HogwartsWebContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hogwarts;
+
+namespace HogwartsWeb.Data
+{
+    public class HogwartsWebContext : DbContext
+    {
+        public HogwartsWebContext (DbContextOptions<HogwartsWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hogwarts.Course> Course { get; set; } = default!;
+        public DbSet<Hogwarts.Enrollment> Enrollment { get; set; } = default!;
+        public DbSet<Hogwarts.House> House { get; set; } = default!;
+        public DbSet<Hogwarts.Pet> Pet { get; set; } = default!;
+        public DbSet<Hogwarts.Professor> Professor { get; set; } = default!;
+        public DbSet<Hogwarts.Spell> Spell { get; set; } = default!;
+        public DbSet<Hogwarts.Student> Student { get; set; } = default!;
+        public DbSet<Hogwarts.Wand> Wand { get; set; } = default!;
+        public DbSet<Hogwarts.WandMaintenanceAlert> WandMaintenanceAlert { get; set; } = default!;
+    }
+}
